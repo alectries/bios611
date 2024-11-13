dc105c664d231ded30eed06abe18353cd7aa3020
alectries <100505370+alectries@users.noreply.github.com>
2024-11-13 15:12:38 -0500

    Added map of all storm events in the continental 48 as a template.

---
+++ scripts/map_events.R
@@ -0,0 +1,54 @@
+# map_events.R
+# Create a map of all storm events in the database, regardless of year and type. Jumping-off point for analysis.
+# requires: source_data/1974_2024-08_stormevents.rds
+
+# Libraries
+require(tidyverse)
+
+# Data
+stormevents <- readRDS("./source_data/1974_2024-08_stormevents.rds")
+
+# Pivot
+stormevents <- stormevents %>% 
+  pivot_longer(
+    cols = c(BEGIN_LAT, BEGIN_LON, END_LAT, END_LON),
+    names_to = 'end',
+    values_to = 'value'
+  ) %>% 
+  mutate(
+    'time' = case_when(
+      str_detect(end, "BEGIN") ~ "begin",
+      str_detect(end, "END") ~ "end",
+      .default = NA
+    ),
+    'll' = case_when(
+      str_detect(end, "LAT") ~ "lat",
+      str_detect(end, "LON") ~ "long",
+      .default = NA
+    ),
+    'value' = as.numeric(value)
+  ) %>% 
+  pivot_wider(
+    id_cols = c(-end),
+    names_from = ll,
+    values_from = value
+  )
+
+# Map
+ggplot() +
+  geom_polygon(
+    data = map_data("state"),
+    aes(x = long, y = lat, group = group),
+    color = "black",
+    fill = "white"
+  ) +
+  geom_point(
+    data = stormevents,
+    aes(x = long, y = lat, group = EVENT_ID),
+    color = "red"
+  ) +
+  coord_map("polyconic",
+            xlim = c(-125, -65),
+            ylim = c(25, 50)) +
+  theme_void()
+  
